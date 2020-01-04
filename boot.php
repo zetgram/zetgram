feedf156755502b6ce7af74130ecef4262ab4ed4
kriptonuz <kripton253@gmail.com>
2020-01-04 18:29:56 +0500

    add debuging

--- boot.php
+++ boot.php
@@ -1,6 +1,11 @@
 <?php
 declare(strict_types=1);
 
+if(getenv('DEBUG') === 'true') {
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+}
+
 use DI\ContainerBuilder;
 
 define('APP_DIR', __DIR__ . '/');
